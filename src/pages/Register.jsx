1cf5b8b861fc56228532f9549cc488b0cc007c74
Anindo <anindo.ah@gmail.com>
2024-05-10 03:09:53 +0600

    register page added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,19 +1,21 @@
 import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/image/logo.jpeg";
-import login from "../assets/image/login.jpg"
-import { AuthContext } from "../Provider/AuthProvider";
+import registerImg from "../assets/image/register.jpg";
 import { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
 import { toast } from "react-hot-toast";
 
-const Login = () => {
-    const {user, userLogin, googleLogin} = useContext(AuthContext);
-    const navigate = useNavigate();
 
+
+const Register = () => {
+    const {user, setUser, createUser, googleLogin, updateUserProfile} = useContext(AuthContext);
+    const navigate = useNavigate();
+    
     // Google login
     const handleGoogleLogin = async() => {
-        try{
+        try {
             await googleLogin();
-            toast.success("Signin Sucessfull");
+            toast.success("Signup Sucessfull");
             navigate('/');
 
         } catch (error){
@@ -22,39 +24,35 @@ const Login = () => {
         }
     }
 
-    // Email login
-    const handleLogin = async e => {
+    // Email registration
+    const handleCreateUser = async e => {
         e.preventDefault();
         const form = e.target;
+        const name =form.name.value;
+        const photo =form.photo.value;
         const email = form.email.value;
         const password = form.password.value;
 
-        const newUser = {email, password};
+        const newUser = {name, photo, email, password};
+        console.log(newUser);
 
         try{
-           const result = await userLogin(email, password);
-           console.log(result.user);
-           toast.success("Signin Sucessfull");
-            navigate('/');
+            const result = await createUser(email, password);
+            console.log(result)
+            await updateUserProfile(name, photo)
+            setUser({ ...user, photoURL: photo, displayName: name })
+            toast.success("Signup Sucessfull");
+            navigate('/home')
         } catch (error){
             console.log(error);
             toast.error(error?.message);
         }
+        
     }
-
-    
 
   return (
     <div className="flex justify-center items-center min-h-[calc(100vh-306px)] my-12">
       <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-4xl ">
-        <div
-          className="hidden bg-cover bg-center lg:block lg:w-1/2"
-          style={{
-            backgroundImage: `url(${login})`,
-          }}
-        >
-        </div>
-
         <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
           <div className="flex justify-center mx-auto">
             <img
@@ -65,7 +63,7 @@ const Login = () => {
           </div>
 
           <p className="mt-3 text-xl text-center text-gray-600 ">
-            Welcome back!
+            Get Your Free Account Now.
           </p>
 
           <div onClick={handleGoogleLogin} className="flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 ">
@@ -99,12 +97,42 @@ const Login = () => {
             <span className="w-1/5 border-b  lg:w-1/4"></span>
 
             <div className="text-xs text-center text-gray-500 uppercase  hover:underline">
-              or login with email
+              or Registration with email
             </div>
 
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
-          <form onSubmit={handleLogin}>
+          <form onSubmit={handleCreateUser}>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="name"
+              >
+                Username
+              </label>
+              <input
+                id="name"
+                autoComplete="name"
+                name="name"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
+            <div className="mt-4">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-600 "
+                htmlFor="photo"
+              >
+                Photo URL
+              </label>
+              <input
+                id="photo"
+                autoComplete="photo"
+                name="photo"
+                className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                type="text"
+              />
+            </div>
             <div className="mt-4">
               <label
                 className="block mb-2 text-sm font-medium text-gray-600 "
@@ -144,7 +172,7 @@ const Login = () => {
                 type="submit"
                 className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
               >
-                Sign In
+                Sign Up
               </button>
             </div>
           </form>
@@ -153,18 +181,24 @@ const Login = () => {
             <span className="w-1/5 border-b  md:w-1/4"></span>
 
             <Link
-              to="/registration"
+              to="/login"
               className="text-xs text-gray-500 uppercase  hover:underline"
             >
-              or sign up
+              or sign in
             </Link>
 
             <span className="w-1/5 border-b  md:w-1/4"></span>
           </div>
         </div>
+        <div
+          className="hidden bg-cover bg-center lg:block lg:w-1/2"
+          style={{
+            backgroundImage: `url(${registerImg})`,
+          }}
+        ></div>
       </div>
     </div>
   );
 };
 
-export default Login;
+export default Register;
